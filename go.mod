464d71687342ab317df1f18c6ee110163f425488
Sergey <seregaan.serega2003@gmail.com>
2023-01-16 21:43:57 +0500

    last-commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sergio1327/in-memory-chache
+
+go 1.19
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"modules/chache"
+	"github.com/Sergio1327/in-memory-chache/chache"
 )
 
 func main() {
-	a := chache.New()
+	a:=chache.New()
 	a.Set("123", "An Sergey Aleksandrovich")
 	a.Get("123")
 	a.Delete("123")
